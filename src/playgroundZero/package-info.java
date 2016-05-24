12b978ec3bda6a8cb69565caebe3696b55badd5a
Rui Wu <contact.rui.wu@gmail.com>
2016-05-24 14:06:21 -0400

    If only commit one file then got problem; try commit all

--- src/playgroundZero/package-info.java
+++ src/playgroundZero/package-info.java
@@ -10,5 +10,8 @@ package playgroundZero;
 
 /**
  * 1. create local project with Eclipse - done
- * 2. create local repository with EGit
+ * 2. create local repository with EGit - done
+ *   - needed to manually commit to make the NO-HEAD tag
+ *     go away
+ * 3. get it on GitHub
  */
