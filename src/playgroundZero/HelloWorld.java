0346cc3948482914abc1544e34457f26cc48b411
Rui Wu <contact.rui.wu@gmail.com>
2016-05-24 15:46:25 -0400

    Just trying out commit and push

--- src/playgroundZero/HelloWorld.java
+++ src/playgroundZero/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// lol I'm not gonna write the Hello World here
+		// remember to add newly created file to index so that it is Git-trackable
 
 	}
 
