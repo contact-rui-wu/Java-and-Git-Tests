74cb3c9cf009c35fb5379303d6cb43ad41365cb2
Rui Wu <contact.rui.wu@gmail.com>
2016-05-24 15:44:46 -0400

    Added HelloWorld.java; for some reason hotkey for commit won't let me
    type in commit message

---
+++ src/playgroundZero/HelloWorld.java
@@ -0,0 +1,11 @@
+package playgroundZero;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// lol I'm not gonna write the Hello World here
+
+	}
+
+}
--- src/playgroundZero/package-info.java
+++ src/playgroundZero/package-info.java
@@ -11,9 +11,13 @@ package playgroundZero;
 /**
  * 1. create local project with Eclipse - done
  * 2. create local repository with EGit - done
- *   - needed to manually commit to make the NO-HEAD tag
- *     go away
+ *   - needed to manually commit to make the NO-HEAD tag go away
  *   - needed to commit all files, else cannot save anymore
- *   - where's that commit button?
- * 3. get it on GitHub
+ *   - where's that commit button? why do I need to write commit message every time?
+ * 3. get it on GitHub - done
+ *   - but every local commit is retained, there's no "push message"
+ *   - so use local commit wisely
+ * 4. make some changes at local and push to remote (GitHub)
+ *   - add a file
+ * 5. make some changes on both sides and resolve conflict at local
  */
