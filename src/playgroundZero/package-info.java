4114f4032442ced0ef793093229a595d13094d65
Rui Wu <contact.rui.wu@gmail.com>
2016-05-24 14:01:48 -0400

    Trying to make the NO-HEAD tag go away

---
+++ src/playgroundZero/package-info.java
@@ -0,0 +1,14 @@
+/**
+ * playgroundZero: creating local project and get it on
+ * GitHub
+ */
+/**
+ * @author rw1679
+ *
+ */
+package playgroundZero;
+
+/**
+ * 1. create local project with Eclipse - done
+ * 2. create local repository with EGit
+ */
