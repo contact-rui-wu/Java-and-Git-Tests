930abd7bfc2cc97c3b95d2c791f95d87663b19c8
Rui Wu <contact.rui.wu@gmail.com>
2016-05-25 09:51:51 -0400

    Testing EGit push: creating new file and then tick the box in commit
    dialog

--- src/playgroundZero/EGitTest_NewFile.java
+++ src/playgroundZero/EGitTest_NewFile.java
@@ -1,7 +1,7 @@
 package playgroundZero;
 
 /**
- * Testing EGit push: add new file
+ * Testing EGit push: add new file and then add to index
  */
 
 public class EGitTest_NewFile {
@@ -9,7 +9,7 @@ public class EGitTest_NewFile {
 	public static void main(String[] args) {
 		// creating new file w/o adding to index - failed
 		// (as expected, new file is not in remote)
-		// adding this file to index then commit
+		// adding this file to index then commit and push - success
 	}
 
 }
---
+++ src/playgroundZero/EGitTest_NewFile2.java
@@ -0,0 +1,14 @@
+package playgroundZero;
+
+/**
+ * Testing EGit push: add new file and then tick the box in commit dialog
+ */
+
+public class EGitTest_NewFile2 {
+
+	public static void main(String[] args) {
+		// nothing
+
+	}
+
+}
