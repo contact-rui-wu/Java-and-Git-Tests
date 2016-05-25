eb44e6705cd3e3edcce85528b43f922da4bbd4e2
Rui Wu <contact.rui.wu@gmail.com>
2016-05-24 20:01:15 -0400

    Clock out

--- src/playgroundZero/HelloWorld.java
+++ src/playgroundZero/HelloWorld.java
@@ -3,10 +3,20 @@ package playgroundZero;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		// lol I'm not gonna write the Hello World here
 		// remember to add newly created file to index so that it is Git-trackable
 
+	}
+	
+	/**
+	 * Nothing, just testing if can use this part as doc;
+	 * yep, can (but how to start a new paragraph?)
+	 * <p>
+	 * Like this
+	 */
+	public static void testDoc() {
+		// nothing
 	}
 
 }
