6437731f242414703130fc4793e9b69a0956d544
Rui Wu <contact.rui.wu@gmail.com>
2016-05-25 09:48:57 -0400

    Testing EGit push: added new file to index (now it appears as ticked in
    commit dialog)

---
+++ src/playgroundZero/EGitTest_NewFile.java
@@ -0,0 +1,15 @@
+package playgroundZero;
+
+/**
+ * Testing EGit push: add new file
+ */
+
+public class EGitTest_NewFile {
+
+	public static void main(String[] args) {
+		// creating new file w/o adding to index - failed
+		// (as expected, new file is not in remote)
+		// adding this file to index then commit
+	}
+
+}
